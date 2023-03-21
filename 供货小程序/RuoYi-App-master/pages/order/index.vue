84772ada1841ded26b20126d1c919485c3fc0256
tian <17764590774@163.com>
2023-03-22 00:00:00 +0800

    增加表格显示

--- 供货小程序/RuoYi-App-master/pages/order/index.vue
+++ 供货小程序/RuoYi-App-master/pages/order/index.vue
@@ -1,17 +1,76 @@
 <template>
-  <view class="content">
-    <image class="logo" src="@/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">Hello wangwang</text>
-    </view>
-  </view>
+  <view class="box">
+              <t-table border="2" border-color="#95b99e" :is-check="true" @change="change">
+                  <t-tr font-size="14" color="#95b99e" align="left">
+                      <t-th align="left">姓名</t-th>
+                      <t-th align="left">年龄</t-th>
+                      <t-th align="left">爱好</t-th>
+                      <t-th align="center">操作</t-th>
+                  </t-tr>
+                  <t-tr font-size="12" color="#5d6f61" align="right" v-for="item in tableList" :key="item.id">
+                      <t-td align="left">{{ item.name }}</t-td>
+                      <t-td align="left">{{ item.age }}</t-td>
+                      <t-td align="left">{{ item.hobby }}</t-td>
+                      <t-td align="left"><button @click="edit(item)">编辑</button></t-td>
+                  </t-tr>
+              </t-table>
+          </view>
+      </view>
 </template>
 
 <script>
-  export default {
-    onLoad: function() {
-    }
-  }
+import tTable from '@/components/t-table/t-table.vue';
+    import tTh from '@/components/t-table/t-th.vue';
+    import tTr from '@/components/t-table/t-tr.vue';
+    import tTd from '@/components/t-table/t-td.vue';
+    export default {
+        components: {
+            tTable,
+            tTh,
+            tTr,
+            tTd
+        },
+        data() {
+            return {
+                tableList: [{
+                        id: 0,
+                        name: '张三',
+                        age: '19',
+                        hobby: '游泳'
+                    },
+                    {
+                        id: 1,
+                        name: '李四',
+                        age: '21',
+                        hobby: '绘画'
+                    },
+                    {
+                        id: 2,
+                        name: '王二',
+                        age: '29',
+                        hobby: '滑板'
+                    },
+                    {
+                        id: 3,
+                        name: '码字',
+                        age: '20',
+                        hobby: '蹦极'
+                    }
+                ]
+            };
+        },
+        methods: {
+            change(e) {
+                console.log(e.detail);
+            },
+            edit(item) {
+                uni.showToast({
+                    title: item.name,
+                    icon: 'none'
+                });
+            }
+        }
+    };
 </script>
 
 <style>
